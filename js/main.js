b2087e93ff6bf44756fd381eb5e62fc23bc7b1d2
sandresbal <s.andresbal@gmail.com>
2018-11-17 16:44:42 +0100

    testing vibration again

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
-var isMobile = (/iPhone|iPod|iPad|Android|BlackBerry/).test(navigator.userAgent);
+/*var isMobile = (/iPhone|iPod|iPad|Android|BlackBerry/).test(navigator.userAgent);
 
 $(".btn, a").on(isMobile ? 'touchend' : 'click', function(e) {
     navigator.vibrate(50);
+});*/
+
+document.getElementById("btn").addEventListener('click', function(){
+  if("vibrate" in navigator){
+    navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;                       
+if (navigator.vibrate) {
+      navigator.vibrate([100, 200, 300]);    
+}    else {
+      alert('I forgot to add your browser');
+    }
+}  else {
+    alert("Vibration is not supported in your browser");  
+    }
 });
